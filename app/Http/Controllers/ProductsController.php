cc405a1f4e20734d6830a39424717f64e932ec86
Charlie Hield <charles.hield@gmail.com>
2025-01-04 16:22:51 -0600

    Update ProductsController to include API token

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -12,7 +12,7 @@ class ProductsController extends Controller
     {
         // Change from sandbox-api.polar.sh -> api.polar.sh when ready to go live
         // And don't forget to update the .env file with the correct POLAR_ORGANIZATION_ID and POLAR_WEBHOOK_SECRET
-        $data = Http::get('https://sandbox-api.polar.sh/v1/products', [
+        $data = Http::withToken(env('POLAR_API_KEY'))->get('https://sandbox-api.polar.sh/v1/products', [
             'organization_id' => env('POLAR_ORGANIZATION_ID'),
             'is_archived' => false,
         ]);
